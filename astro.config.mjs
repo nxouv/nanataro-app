9d8aeb52701fdb3d29962893aab8d605fdca0ee4
nxouv <nanataro.nx@gmail.com>
2025-12-09 14:17:24 +0900

    Add sitemap integration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import embeds from 'astro-embed/integration';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://nanataro.app',
-  integrations: [embeds(), mdx()],
+  integrations: [embeds(), mdx(), sitemap()],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
